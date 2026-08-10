3f3cf81382a7a5518537666741f0b7db18572fa1
Sukhvinder Panesar <sukypanesar@gmail.com>
2026-08-10 09:47:55 +0100

    feat: add brownface logo v7.6.4

--- autoteams-web/.patch-backups/v7.7.0/page-20260810-094054.tsx
+++ autoteams-web/.patch-backups/v7.7.0/page-20260810-094054.tsx
@@ -1,4 +1,3 @@
-import { BrownfaceProject } from "@/components/landing/BrownfaceProject";
 import Link from "next/link";
 import { PageShell } from "@/components/Site";
 
@@ -888,9 +887,7 @@ export default function HomePage() {
             </div>
           </div>
         </section>
-      
-      <BrownfaceProject />
-</main>
+      </main>
     </PageShell>
   );
 }
